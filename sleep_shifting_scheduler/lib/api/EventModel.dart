fc08e1f320af85db93b0213b8608270326a97b5f
Blaise <blaisetiong1030@gmail.com>
2020-10-03 13:32:47 +0800

    EventListModel and EventModel completed

---
+++ sleep_shifting_scheduler/lib/api/EventModel.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_calendar_carousel/classes/event.dart';
+import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart';
+
+class EventListModel {
+  // EventList<Event> is used in calender;
+  EventList<Event> thisEventsList;
+  Map<DateTime, List<EventModel>> thisEventsModelMap;
+  Map<String, List<Map<String, dynamic>>> fromMap;
+
+  EventListModel({this.thisEventsModelMap, this.fromMap}) {
+    // Initialize the ListModel with Map from database
+    if (fromMap != null) {
+      thisEventsList = EventList(
+        events: fromMap.map(
+          (String date, List<Map<String, dynamic>> value) => MapEntry(
+            DateTime.parse(date),
+            value
+                .map((Map<String, dynamic> e) =>
+                    EventModel(fromMap: e).thisEvent)
+                .toList(),
+          ),
+        ),
+      );
+    } else {
+      // Generate the EventList from thisEventsModelMap when we dont have Map from Database
+      thisEventsList = EventList(
+        events: thisEventsModelMap.map(
+          (DateTime key, List<EventModel> value) => MapEntry(
+            key,
+            value.map((EventModel e) => e.thisEvent).toList(),
+          ),
+        ),
+      );
+    }
+  }
+  // Generates the map
+  Map<String, List<Map<String, dynamic>>> toMap() {
+    return thisEventsModelMap.map(
+      (DateTime key, List<EventModel> value) => MapEntry(
+        key.toIso8601String(),
+        value.map((EventModel e) => e.getMap()).toList(),
+      ),
+    );
+  }
+
+  EventList<Event> get eventList => thisEventsList;
+}
+
+class EventModel {
+  /* A single event object*/
+  String eventId;
+  String eventTitle;
+  String description;
+  DateTime dateTime;
+  String color;
+  Map<String, dynamic> fromMap;
+  // Event is used in the calender
+  Event thisEvent;
+
+  EventModel({
+    this.color,
+    this.eventTitle,
+    this.description,
+    this.dateTime,
+    this.eventId,
+    this.fromMap,
+  }) {
+    // We can put in a Map from database to build this object
+    if (fromMap != null) {
+      eventId = fromMap['eventId'];
+      eventTitle = fromMap['eventTitle'];
+      description = fromMap['description'];
+      dateTime = DateTime.parse(fromMap['dateTime']);
+      color = fromMap['color'];
+    }
+    // otherwise we just need to create the objects by passing in the params
+    initEvent();
+  }
+
+  // Initializes Event object which will be used in the calender widget
+  void initEvent() => thisEvent = Event(
+        date: dateTime,
+        icon: Icon(Icons.add),
+        dot: Container(
+          margin: EdgeInsets.symmetric(horizontal: 1.0),
+          color: colorFromString(),
+          height: 5.0,
+          width: 5.0,
+        ),
+      );
+
+  // Converts color from hexString to Color
+  Color colorFromString() {
+    final buffer = StringBuffer();
+    if (color.length == 6 || color.length == 7) buffer.write('ff');
+    buffer.write(color.replaceFirst('#', ''));
+    return Color(int.parse(buffer.toString(), radix: 16));
+  }
+
+  // Generates a map object
+  Map<String, dynamic> getMap() => {
+        'eventId': eventId,
+        'eventTitle': eventTitle,
+        'color': color,
+        'dateTime': dateTime.toIso8601String(),
+        'description': description,
+      };
+
+  //Some other getters
+  Event get event => thisEvent;
+  String get eventDescription => description;
+  Color get eventEolor => colorFromString();
+  DateTime get eventTime => dateTime;
+  String get currentEventId => eventId;
+}
--- sleep_shifting_scheduler/lib/widgets/calendar.dart
+++ sleep_shifting_scheduler/lib/widgets/calendar.dart
@@ -5,6 +5,7 @@ import 'package:flutter_calendar_carousel/classes/event_list.dart';
 import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart'
     show CalendarCarousel;
 import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart';
+import 'package:sleep_shifting_scheduler/api/EventModel.dart';
 
 class CalenderWidget extends StatefulWidget {
   CalenderWidget({Key key}) : super(key: key);
@@ -31,36 +32,52 @@ class _CalenderWidgetState extends State<CalenderWidget> {
   ];
   DateTime _currentDate = DateTime.now();
   int scrollMonth = DateTime.now().month;
-  EventList _markedDateMap = EventList<Event>(
-    events: {
-      new DateTime(2019, 2, 10): [
-        new Event(
-          date: new DateTime(2019, 2, 10),
-          title: 'Event 1',
-          icon: Icon(Icons.add_a_photo_rounded),
-          dot: Container(
-            margin: EdgeInsets.symmetric(horizontal: 1.0),
-            color: Colors.red,
-            height: 5.0,
-            width: 5.0,
-          ),
-        ),
-        new Event(
-          date: new DateTime(2019, 2, 10),
-          title: 'Event 2',
-          icon: Icon(Icons.ac_unit),
-        ),
-        new Event(
-          date: new DateTime(2019, 2, 10),
-          title: 'Event 3',
-          icon: Icon(Icons.ac_unit),
-        ),
-      ],
-    },
-  );
+
+  EventList<Event> _markedDateMap = EventListModel(fromMap: {
+    DateTime(2020, 10, 5).toIso8601String(): [
+      {
+        'eventId': '1',
+        'eventTitle': 'hello world',
+        'color': '#aabbcc',
+        'dateTime': DateTime.now().toIso8601String(),
+      },
+      {
+        'eventId': '2',
+        'eventTitle': 'hello world',
+        'color': '#aabbcc',
+        'dateTime': DateTime.now().toIso8601String(),
+      },
+      {
+        'eventId': '3',
+        'eventTitle': 'hello world',
+        'color': '#aabbcc',
+        'dateTime': DateTime.now().toIso8601String(),
+      }
+    ],
+    DateTime(2020, 10, 4).toIso8601String(): [
+      {
+        'eventId': '1',
+        'eventTitle': 'hello world',
+        'color': '#aabbcc',
+        'dateTime': DateTime.now().toIso8601String(),
+      },
+      {
+        'eventId': '2',
+        'eventTitle': 'hello world',
+        'color': '#aabbcc',
+        'dateTime': DateTime.now().toIso8601String(),
+      },
+      {
+        'eventId': '3',
+        'eventTitle': 'hello world',
+        'color': '#aabbcc',
+        'dateTime': DateTime.now().toIso8601String(),
+      }
+    ]
+  }).eventList;
+
   @override
   Widget build(BuildContext context) {
-    var size = MediaQuery.of(context).size;
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
@@ -81,43 +98,18 @@ class _CalenderWidgetState extends State<CalenderWidget> {
               SizedBox(height: 23),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Text('Sun',
-                          style: TextStyle(fontSize: 16, color: Colors.white))),
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text('Mon',
-                        style: TextStyle(fontSize: 16, color: Colors.white)),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text('Tue',
-                        style: TextStyle(fontSize: 16, color: Colors.white)),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text('Wed',
-                        style: TextStyle(fontSize: 16, color: Colors.white)),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text('Thu',
-                        style: TextStyle(fontSize: 16, color: Colors.white)),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text('Fri',
-                        style: TextStyle(fontSize: 16, color: Colors.white)),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text('Sat',
-                        style: TextStyle(fontSize: 16, color: Colors.white)),
-                  ),
-                ],
-              )
+                children: (['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'])
+                    .map(
+                      (e) => Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          e,
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ),
             ],
           ),
         ),
