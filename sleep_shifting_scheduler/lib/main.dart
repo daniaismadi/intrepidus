56a17b4563d6961b2f18b65a446ff15d6ca24402
Dania Ismadi <dism0001@student.monash.edu>
2020-10-03 13:50:28 +0800

    Added logic for firebase.

--- sleep_shifting_scheduler/lib/main.dart
+++ sleep_shifting_scheduler/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sleep_shifting_scheduler/database/database.dart';
 
 void main() {
   runApp(MyApp());
@@ -63,6 +64,12 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void createData() {
+    databaseReference
+        .child("userID")
+        .set({'name': 'Dania Ismadi', 'description': 'Astronaut'});
+  }
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -108,7 +115,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: createData,
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
