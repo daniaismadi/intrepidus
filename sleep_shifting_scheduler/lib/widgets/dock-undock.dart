f401088d6c48bf24e1197dc48a2e1885a32dbfc8
Dania Ismadi <dism0001@student.monash.edu>
2020-10-04 10:53:59 +0800

    Fixed justification for dock-undock page.

--- sleep_shifting_scheduler/lib/widgets/dock-undock.dart
+++ sleep_shifting_scheduler/lib/widgets/dock-undock.dart
@@ -14,59 +14,68 @@ class _DockingWidgetState extends State<DockingWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        shrinkWrap: true,
-        padding: const EdgeInsets.only(top: 130),
-        children: [
-          Center(
-              child: Container(
-                  // margin: new EdgeInsets.fromLTRB(20, 40, 20, 10),
-                  padding: new EdgeInsets.fromLTRB(5, 20, 5, 20),
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                          color: Color.fromRGBO(7, 78, 232, 1.0), width: 2),
-                      borderRadius: BorderRadius.all(Radius.circular(50))),
-                  alignment: Alignment(0, 0),
-                  height: 360,
-                  width: 300,
-                  child: buildFields())),
-          SizedBox(height: 80),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  SvgPicture.asset('lib/images/intrepiduslogo180px.svg',
-                      width: 90, height: 90)
-                ]),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Container(
+        margin: new EdgeInsets.fromLTRB(0, 0, 0, 30),
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          // shrinkWrap: true,
+          // padding: const EdgeInsets.only(top: 130),
+          children: [
+            Center(
+                child: Container(
+                    margin: new EdgeInsets.fromLTRB(20, 100, 20, 10),
+                    padding: new EdgeInsets.fromLTRB(5, 20, 5, 20),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                            color: Color.fromRGBO(7, 78, 232, 1.0), width: 2),
+                        borderRadius: BorderRadius.all(Radius.circular(50))),
+                    alignment: Alignment(0, 0),
+                    height: 360,
+                    width: 300,
+                    child: buildFields())),
+            // Bottom widgets.
+            Column(
               children: [
-                IconButton(
-                    icon: new Icon(CupertinoIcons.arrow_left,
-                        color: Color.fromRGBO(7, 78, 232, 1.0)),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    }),
-                RaisedButton(
-                    onPressed: () {},
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5.0)),
-                    textColor: Colors.white,
-                    padding: const EdgeInsets.all(0.0),
-                    color: Color.fromRGBO(7, 78, 232, 1.0),
-                    child: Container(
-                        padding: const EdgeInsets.all(10),
-                        child: const Text('Confirm',
-                            style: TextStyle(
-                                fontFamily: 'Segoe-UI', fontSize: 15))))
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SvgPicture.asset('lib/images/intrepiduslogo180px.svg',
+                            width: 90, height: 90)
+                      ]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                          icon: new Icon(CupertinoIcons.arrow_left,
+                              color: Color.fromRGBO(7, 78, 232, 1.0)),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          }),
+                      RaisedButton(
+                          onPressed: () {},
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5.0)),
+                          textColor: Colors.white,
+                          padding: const EdgeInsets.all(0.0),
+                          color: Color.fromRGBO(7, 78, 232, 1.0),
+                          child: Container(
+                              padding: const EdgeInsets.all(10),
+                              child: const Text('Confirm',
+                                  style: TextStyle(
+                                      fontFamily: 'Segoe-UI', fontSize: 15))))
+                    ],
+                  ),
+                )
               ],
             ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
