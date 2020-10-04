1eebfc9670271280caccd4833740a253543c0e3d
Blaise <blaisetiong1030@gmail.com>
2020-10-04 13:14:58 +0800

    Checking out

--- sleep_shifting_scheduler/lib/widgets/console-shifts.dart
+++ sleep_shifting_scheduler/lib/widgets/console-shifts.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:sleep_shifting_scheduler/widgets/custom_widget/pickDate.dart';
 import 'package:sleep_shifting_scheduler/widgets/custom_widget/pickTime.dart';
 import 'package:sleep_shifting_scheduler/widgets/dock-undock.dart';
